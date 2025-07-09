2db731c70c2ecbdb1c52ee59b3dce545f2e375ad
Haydar <ahadyahaydar@gmail.com>
2025-07-09 16:01:42 +0700

    menambahkan Controller.php dan memperbaiki import di AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
